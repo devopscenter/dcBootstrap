0c311581924b4663c5bd26db26ab99703ec04fdd
Gregg Jensen <gjensen@devops.center>
2018-07-30 13:39:32 -0500

    remove all setup for AWS as a default for a user.  If a user needs AWS access they will need to run createIAMUsers.sh or at minimum set up AWS credentials on their local machine.

--- RUN-ME-FIRST.sh
+++ RUN-ME-FIRST.sh
@@ -15,7 +15,7 @@
 #                   - create the directory if it doesn't exist
 #                   - cd to that directory 
 #                   - clone dcUtils
-#                   - then 
+#                   - then ...
 #           
 # 
 #       OPTIONS: ---
@@ -68,25 +68,6 @@ getBasePath()
 
 
 #---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  determineProfile
-#   DESCRIPTION:  will check to see if the .aws/config is on the default or a real-profile
-#    PARAMETERS:  
-#       RETURNS:  
-#-------------------------------------------------------------------------------
-determineProfile()
-{
-    aProfile=$(aws configure get region --profile ${PROFILE} 2>&1 > /dev/null)
-    if [[ ${aProfile} == *"could not be found"* ]]; then
-        anotherProfile=$(aws configure get region --profile default)
-        if [[ ${anotherProfile} == "us-west-2" ]] || [[ ${anotherProfile} == "us-east-1" ]]; then
-            INITIAL_PROFILE="default"
-        fi
-    else
-        INITIAL_PROFILE=${PROFILE}
-    fi
-}
-
-#---  FUNCTION  ----------------------------------------------------------------
 #          NAME:  copyLoggingScript
 #   DESCRIPTION:  copy dcUtils/script/dcEnv.sh to destination directory
 #    PARAMETERS:  
@@ -113,83 +94,6 @@ copyLoggingScript()
     fi
 }
 
-#---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  setupIAMUser
-#   DESCRIPTION:  creates the IAM user under a specific group which defines the rolse
-#                 for this user.  
-#    PARAMETERS:  
-#       RETURNS:  
-#-------------------------------------------------------------------------------
-setupIAMUser()
-{
-    echo "Setting up this user as an IAM user"
-
-    # The group name that was set up at customer registration time.  There should be a group that would
-    # be named CUSTOMER_NAME-dev and the policy for the group is to have EC2 and S3 full read/write access
-    # but not admin priviledge.  That is left to the authenticated user(s)
-    IAMUserGroup="${CUSTOMER_NAME}-dev"
-        
-
-	IAMUserToCreate=${USER_NAME}
-    if [[ $(aws --profile ${INITIAL_PROFILE} --region ${REGION} iam get-user --user-name ${IAMUserToCreate} 2>&1 > /dev/null) == *"cannot be found"* ]]; then
-        sleep 2
-        createUser=$(aws --profile ${INITIAL_PROFILE} --region ${REGION} iam create-user --user-name  ${IAMUserToCreate} 2>&1 > /dev/null)
-        sleep 2
-
-        # create the keys for the user
-        accessKeys=$(aws --profile ${INITIAL_PROFILE} --region ${REGION} iam create-access-key --user-name  ${IAMUserToCreate})
-        SECRET_ACCESS_KEY=$(jq -r '.AccessKey.SecretAccessKey' <<< "$accessKeys")
-        ACCESS_KEY=$(jq -r '.AccessKey.AccessKeyId' <<< "$accessKeys")
-        sleep 2
-
-        # add the login to a group so that the policy can be attached to the group rather  than the user
-        addToGroup=$(aws --profile ${INITIAL_PROFILE} --region ${REGION} iam add-user-to-group --user-name ${IAMUserToCreate} --group-name ${IAMUserGroup} 2>&1 > /dev/null)
-        sleep 2
-    else
-        # add the login to a group so that the policy can be attached to the group rather than the user
-        addToGroup=$(aws --profile ${INITIAL_PROFILE} --region ${REGION} iam add-user-to-group --user-name ${IAMUserToCreate} --group-name ${IAMUserGroup} 2>&1 > /dev/null)
-        sleep 2
-
-        # create the keys for the user
-        accessKeys=$(aws --profile ${INITIAL_PROFILE} --region ${REGION} iam create-access-key --user-name  ${IAMUserToCreate})
-        SECRET_ACCESS_KEY=$(jq -r '.AccessKey.SecretAccessKey' <<< "$accessKeys")
-        ACCESS_KEY=$(jq -r '.AccessKey.AccessKeyId' <<< "$accessKeys")
-        sleep 2
-
-    fi
-}
-
-
-#---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  runAWSConfigure
-#   DESCRIPTION:  runs the aws configure --profile command with the appropriate informaitno
-#                 this constitutes running several aws configure set commands with the required data
-#    PARAMETERS:  
-#       RETURNS:  
-#-------------------------------------------------------------------------------
-runAWSConfigure()
-{
-    echo "Setting up this users AWS configuration"
-
-    if [[ -z ${ACCESS_KEY} ]]; then
-        echo
-        echo "** NOTE **"
-        echo "The keys could not be created so the .aws/credentials could not be updated"
-        echo "This will have to be corrected manually before any additional devops.center"
-        echo "scripts can be run."
-        echo
-    else
-        aws configure set aws_access_key_id ${ACCESS_KEY} --profile ${PROFILE}
-        sleep 2
-        aws configure set aws_secret_access_key ${SECRET_ACCESS_KEY} --profile ${PROFILE}
-        sleep 2
-        aws configure set region ${REGION} --profile ${PROFILE}
-        sleep 2
-        aws configure set output json --profile ${PROFILE}
-        sleep 2
-    fi
-}
-
 
 #---  FUNCTION  ----------------------------------------------------------------
 #          NAME:  createUserSpecificKeys
@@ -204,47 +108,11 @@ createUserSpecificKeys()
         mkdir -p ~/.ssh/devops.center
     fi
 
-    # call the ssh-keygen to create a private/public set of keys
-    echo "Creating ssh access keys (dcaccess-key) to be used to access the AWS instances"
-    ssh-keygen -t rsa -N "" -f ~/.ssh/devops.center/dcaccess-key -q
-    mv ~/.ssh/devops.center/dcaccess-key ~/.ssh/devops.center/dcaccess-key.pem
-
     # and create a separate key for the dcAuthorization server
     ssh-keygen -t rsa -N "" -f ~/.ssh/devops.center/dcauthor-${USER_NAME}-key -q
     mv ~/.ssh/devops.center/dcauthor-${USER_NAME}-key ~/.ssh/devops.center/dcauthor-${USER_NAME}-key.pem
 }
 
-
-#---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  sendKeysTodc
-#   DESCRIPTION:  sends the public key to devops.center,whom will distribute to the 
-#                 customer instances
-#    PARAMETERS:  
-#       RETURNS:  
-#-------------------------------------------------------------------------------
-sendKeysTodc()
-{
-    # TODO: when creating the groups that will be used by the IAM users, need to create
-    #       one that has the policies: IAMUserSSHKeys
-    #       this group will be used for the script in this function to upload the ssh public
-    #       keys to the IAM role
-    #       PLACEHOLDER name for the group: public-key-transfer
-
-    echo "And now we need to associate the public key with the new IAM user"
-    echo
-    # send the public key to the IAM user just created and devops.center will disseminate
-    # it to the appropriate instances
-    # add group public-key-tranfer to the IAM user
-    aws --profile ${INITIAL_PROFILE} --region ${REGION} iam add-user-to-group --user-name ${USER_NAME} --group-name public-key-transfer 2>&1 > /dev/null
-    sleep 2
-
-    # upload the public key
-    UPLOAD=$(aws --profile ${INITIAL_PROFILE} --region ${REGION} iam upload-ssh-public-key --user-name ${USER_NAME} --ssh-public-key-body "$(cat ~/.ssh/devops.center/dcaccess-key.pub) 2>&1 > /dev/null")
-    sleep 2
-
-    # remove the group public-key-tranfer from the IAM user
-    aws --profile ${INITIAL_PROFILE} --region ${REGION} iam remove-user-from-group --user-name ${USER_NAME} --group-name public-key-transfer 2>&1 > /dev/null
-}
 
 
 #---  FUNCTION  ----------------------------------------------------------------
@@ -256,7 +124,7 @@ sendKeysTodc()
 #-------------------------------------------------------------------------------
 writeToSettings()
 {
-    echo "dcCloudService=AWS" > ~/.dcConfig/settings
+    echo "dcCloudService=NA" > ~/.dcConfig/settings
     echo "dcUTILS=${dcUTILS}" >> ~/.dcConfig/settings
     echo "CUSTOMER_NAME=${CUSTOMER_NAME}" >> ~/.dcConfig/settings
     echo "PROFILE=${PROFILE}" >> ~/.dcConfig/settings
@@ -326,108 +194,6 @@ EOF
         echo "GIT_SERVICE_NAME=${GIT_SERVICE_NAME}" >> ${SHARED_CONFIG_DIR}/settings
         echo "GIT_ACCOUNT_NAME=${GIT_ACCOUNT_NAME}" >> ${SHARED_CONFIG_DIR}/settings
     fi
-}
-
-
-#---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  cleanUpAWSConfigs
-#   DESCRIPTION:  remove the bootstrap section from the .aws/{config|credentials}
-#    PARAMETERS:  
-#       RETURNS:  
-#-------------------------------------------------------------------------------
-cleanUpAWSConfigs()
-{
-    cd ~/.aws
-
-    # do a diff and grab the lines that are different in the new one
-    if [[ $(diff config.OLD config) ]]; then 
-        echo "[profile ${PROFILE}]" > config.NEW
-        diff config.OLD config | grep '^>' | sed 's/^>\ //' >> config.NEW
-    else
-        cp config config.NEW
-    fi
-    echo "[${PROFILE}]" > credentials.NEW
-    diff credentials.OLD credentials | grep '^>' | sed 's/^>\ //' >> credentials.NEW
-
-    #-------------------------------------------------------------------------------
-    # make the NEW ones the ones to keep
-    #-------------------------------------------------------------------------------
-    mv config.NEW config
-    mv credentials.NEW credentials
-
-    #-------------------------------------------------------------------------------
-    # and remove the OLD ones
-    #-------------------------------------------------------------------------------
-    rm config.OLD credentials.OLD
-
-    if [[ ${ALREADY_HAS_AWS_CONFIGS} == "yes" ]]; then
-        # they had an original config and credentials.  Append the new files to the 
-        # original ones
-        alreadyHasEntry=$(grep $PROFILE ~/.aws/config.ORIGINAL)
-        if [[ -z ${alreadyHasEntry} ]]; then
-            cat config >> config.ORIGINAL
-            cat credentials >> credentials.ORIGINAL
-        else
-            # they already have a profile by this name so make a second one
-            echo 
-            echo "There was already an entry in the original .aws/config for the "
-            echo "profile (${PROFILE} you are creating.  So, the entry will be marked"
-            echo "as ${PROFILE}-2 and you might need to do some manual work to clean"
-            echo "this up or work with the new name of the profile."
-
-            sed -i "s/${PROFILE}/${PROFILE}-2/" config
-            sed -i "s/${PROFILE}/${PROFILE}-2/" credentials
-
-            # and append them to the original file
-            cat config >> config.ORIGINAL
-            cat credentials >> credentials.ORIGINAL
-        fi
-
-        # and move the original back to the only copy left in the .aws directory.
-        mv config.ORIGINAL config
-        mv credentials.ORIGINAL credentials
-    fi
-}
-
-
-#---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  bootstrapAWSConfigs
-#   DESCRIPTION:  
-#    PARAMETERS:  
-#       RETURNS:  
-#-------------------------------------------------------------------------------
-bootstrapAWSConfigs()
-{
-    ALREADY_HAS_AWS_CONFIGS="no"
-
-    if [[ -f $HOME/.aws/credentials ]]; then
-        # copy the original config and credentials so we don't bother them
-        mv ~/.aws/config ~/.aws/config.ORIGINAL
-        mv ~/.aws/credentials ~/.aws/credentials.ORIGINAL
-
-        # set this to yes, meaning that they already had a config/credentials file so do NOT
-        # clean it up
-        ALREADY_HAS_AWS_CONFIGS="yes"
-    fi
-
-    if [[ -f "${BASE_DIR}/bootstrap-aws.tar" ]]; then
-        cd $HOME
-        tar -xf "${BASE_DIR}/bootstrap-aws.tar"
-        cp ~/.aws/config ~/.aws/config.OLD
-        cp ~/.aws/credentials ~/.aws/credentials.OLD
-    else
-        echo 
-        echo "Could not find the bootstrap-aws tar ball which is required to begin"
-        echo "this script.  Contact the devops.center representative to ensure that"
-        echo "the file is created and put into the directory: ${BASE_DIR}"
-        echo
-        exit 1
-    fi
-
-    #-------------------------------------------------------------------------------
-    # NOTE: Don't forget to remove the bootstrap sections out of the config and credentials
-    # when this script is done
-    #-------------------------------------------------------------------------------
 }
 
 
@@ -551,34 +317,6 @@ if [[ $BV != "4"* ]]; then
 fi
 
 #-------------------------------------------------------------------------------
-# need to check to see if aws and jq have been loaded and if not install them
-#-------------------------------------------------------------------------------
-CHECK_AWS=$(which aws)
-if [[ ! ${CHECK_AWS} ]]; then
-    echo 
-    echo "The devops.center scripts will use the AWS cli commands to access AWS."
-    echo "The command 'aws' does not appear to be installed on your machine."
-    echo "Note that the aws cli is installed via the python installation process"
-    echo "called: pip.  This requires python to be installed with version 2.7+."
-    echo "(python version 2.7+ is what the devops.center scripts use)"
-    echo 
-    exit 1
-fi
-
-CHECK_JQ=$(which jq)
-if [[ ! ${CHECK_JQ} ]]; then
-    echo 
-    echo "The devops.center scripts will use the aws cli commands to access AWS"
-    echo "and then the jq command (jq is a json output interpreter) to cull"
-    echo "additional information from the results that come from the aws command."
-    echo "The command 'jq' does not appear to be installed on your machine."
-    echo "Please use your normal installation method for intalling new"
-    echo "software to install the command 'jq'."
-    echo 
-    exit 1
-fi
-
-#-------------------------------------------------------------------------------
 # get some details to go into settings:
 #     customer name
 #     user name
@@ -620,7 +358,6 @@ PROFILE=${CUSTOMER_NAME}
 #-------------------------------------------------------------------------------
 echo 
 echo "Enter your username, one word, no spaces and all lowercase letters."
-echo "This value will be used to create a AWS IAM user specifically for you."
 echo 
 
 # check to see if we have a default value
@@ -660,31 +397,6 @@ if [[ ${sharedDrivePath} == "~"* || ${sharedDrivePath} == "\$HOME"* ]]; then
 else
     dcCOMMON_SHARED_DIR=${sharedDrivePath}
 fi
-
-
-#-------------------------------------------------------------------------------
-# if using AWS get the region for the instances
-#-------------------------------------------------------------------------------
-echo 
-echo "If the cloud environment you are using is AWS, enter the region that the cloud "
-echo "instances will be in when they are created. This value can be obtained from the"
-echo "main authentication user if it is not known. "
-echo "(The value is typically us-west-2 or us-east-1.)"
-echo 
-
-# check to see if we have a default value
-if [[ ${REGION} ]]; then
-    read -i ${REGION} -p "Enter the region and press [ENTER]: " -e region
-else
-    read -i us-west-2  -p "Enter the region and press [ENTER]: " -e region
-fi
-
-if [[ -z ${region} ]]; then
-    REGION=us-west-2
-else
-    REGION=${region,,}
-fi
-
 
 #-------------------------------------------------------------------------------
 # get which git service the customer is using and their repository account name
@@ -759,34 +471,10 @@ DEV_BASE_DIR=${localDevBaseDir}
 clonedcUtils
 
 #-------------------------------------------------------------------------------
-# now we need to get the bootstrap aws config and credentials to be able to set
-# up the IAM user
-#-------------------------------------------------------------------------------
-bootstrapAWSConfigs
-
-#-------------------------------------------------------------------------------
-# need to help them run through setting up the IAM user for this user and use 
-# the AccessKey and SecretKey that is created specifically for this user to be 
-# used in the aws configure setup. 
-#-------------------------------------------------------------------------------
-determineProfile
-setupIAMUser
-
-#-------------------------------------------------------------------------------
 # create the personal private access key to authenticate ssh to an instance 
 # ... put it in the .ssh/devops.center directory or the ~/.dcConfig/ directory
 #-------------------------------------------------------------------------------
 createUserSpecificKeys
-
-#-------------------------------------------------------------------------------
-# and make the shared key available to devops.center 
-#-------------------------------------------------------------------------------
-sendKeysTodc
-
-#-------------------------------------------------------------------------------
-# run aws configure with the appropriate information gathered so far
-#-------------------------------------------------------------------------------
-runAWSConfigure
 
 #-------------------------------------------------------------------------------
 # we have collected all the information we need now write it out to .dcConfig/settings
@@ -799,13 +487,6 @@ writeToSettings
 # better output. 
 #-------------------------------------------------------------------------------
 copyLoggingScript
-
-#-------------------------------------------------------------------------------
-# Now is the time to remove the bootstrap section from the .aws/{config|credentials}
-# this will leave the config and credentials with the PROFILE specifc information and
-# their specific credentials in the ~/.aws/ config files.
-#-------------------------------------------------------------------------------
-cleanUpAWSConfigs
 
 #-------------------------------------------------------------------------------
 # tell the user to add path to dcUtils to the $PATH
